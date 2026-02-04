5409185e73f3303d91277c27ea4dcee6018cda67
yehorkardash <yehor.kardash@n8n.io>
2026-02-04 15:40:35 +0100

    remove old folder

--- packages/@n8n/ai-node-sdk/src/creators/create-chat-model.ts
+++
@@ -1,6 +0,0 @@
-import { LangchainAdapter } from 'src/adapters/langchain';
-import type { ChatModel } from 'src/types/chat-model';
-
-export function createChatModel(model: ChatModel) {
-	return new LangchainAdapter(model);
-}
