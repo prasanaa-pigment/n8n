53fed9eae2212163f1d9d67f3514356b71e7debb
yehorkardash <yehor.kardash@n8n.io>
2026-02-09 12:48:00 +0100

    include metadata

--- packages/@n8n/ai-utilities/src/adapters/langchain-chat-model.ts
+++ packages/@n8n/ai-utilities/src/adapters/langchain-chat-model.ts
@@ -86,6 +86,12 @@ export class LangchainAdapter<
 			lcMessage.usage_metadata = usage_metadata;
 		}
 
+		lcMessage.response_metadata = {
+			...result.providerMetadata,
+			model: this.chatModel.modelId,
+			provider: this.chatModel.provider,
+		};
+
 		return {
 			generations: [
 				{
